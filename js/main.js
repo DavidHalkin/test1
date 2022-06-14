6161c4f208f2054ba70a46f0310ad4a01ced79e0
David Halkin <halkin.david@gmail.com>
2022-06-14 04:09:26 +0300

    test

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    let asd="asd";
+    $(".toggle_js").click(function () {
+        $(this).toggleClass("active");
+        // $(".nav_holder_js").toggleClass("opened");
+        return false;
+    });
+});
